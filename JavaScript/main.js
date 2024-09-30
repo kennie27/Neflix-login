993c2fd3f0459698a61d108590a954a67e8793e0
kennie27 <nderuken@gmail.com>
2024-09-30 11:29:00 +0300

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,5 @@
+document.getElementById("signin").onclick = function() {
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    
+}
